3ba34f6368057c110f281677ae330d138be442ad
Allen CL Lin <allen655@Allens-MBP.local>
2018-03-05 09:51:29 +0800

    Add WPF Common Library

---
+++ Data/ExcelFetcher.cs
@@ -0,0 +1,59 @@
+﻿using POCQL;
+using POCQL.DAO;
+using POCQL.DAO.ConnectionObject;
+using POCQL.Model.MapAttribute;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WPF.Common.Library.Model;
+
+namespace WPF.Common.Library.Data
+{
+    public class ExcelFetcher : PropertyBinder<ExcelFetcher>
+    {
+        public string FilePath { get { return _PropBind.GetValue<string>(); } }
+
+        /// <summary>
+        /// DAO Factory
+        /// </summary>
+        private static Factory DaoFactory
+        {
+            get
+            {
+                if (!File.Exists(_Entity.FilePath)) throw new FileNotFoundException();
+
+                return new OledbFactory
+                {
+                    DataSource = _Entity.FilePath,
+                    Provider = "Microsoft.ACE.OLEDB.12.0",
+                    ExtendedProperties = @"Excel 12.0;HDR=Yes;IMEX=1"
+                };
+            }
+        }
+
+        /// <summary>
+        /// Data Access Object
+        /// </summary>
+        private static DataAccess Access { get { return new POCQL.DAO.DataAccess(DaoFactory); } }
+
+        /// <summary>
+        /// Fetch Excel Sheet of Datas
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<T> FetchSheetDatas<T>()
+            where T : class, IExcelSheet
+        {
+            Type type = typeof(T);
+            string table = type.GetCustomAttribute<EntityMapperAttribute>().MainTable;
+            return Select.Columns<T>()
+                     .From(table, true)
+                     .Where(type.FilterCondition())
+                     .Query<T>(Access);
+        }
+    }
+}
---
+++ Process/Repeat.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WPF.Common.Library.Process
+{
+    /// <summary>
+    /// 重複嘗試者
+    /// </summary>
+    public class Repeater
+    {
+        /// <summary>
+        /// 重複嘗試的Timer
+        /// </summary>
+        private Timer _Timer;
+        /// <summary>
+        /// 重複嘗試最高次數
+        /// </summary>
+        private int MaxRepeatTimes = 0;
+        /// <summary>
+        /// 已經嘗試次數
+        /// </summary>
+        private int RepeatTime = 0;
+
+        /// <summary>
+        /// 是否已經達到最高次數
+        /// </summary>
+        public bool IsTimeUp => RepeatTime >= MaxRepeatTimes;
+        /// <summary>
+        /// Timer是否已經開始
+        /// </summary>
+        public bool IsStarted => _Timer?.Enabled ?? false;
+
+        public Repeater(double interval, int maxRepeat = 0)
+        {
+            _Timer = new Timer
+            {
+                Interval = interval
+            };
+
+            MaxRepeatTimes = maxRepeat;
+        }
+
+        /// <summary>
+        /// 重複執行動作
+        /// </summary>
+        /// <param name="func">要重複執行的動作</param>
+        public void Repeat(Func<bool> func)
+        {
+            RepeatTime = 0;
+            _Timer.Elapsed += new ElapsedEventHandler((s, e) =>
+            {
+                // *** 如果要重複執行的動作已經成功或是達最高上限就要關閉Timer ***
+                if (func() || (MaxRepeatTimes != 0 && ++RepeatTime > MaxRepeatTimes)) _Timer.Close();
+            });
+
+            _Timer.Start();
+        }
+    }
+}
